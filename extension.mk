aaf205772570ce2d43f201ed50e0c11556af4907
Giovanni Campagna <gcampagna@src.gnome.org>
2011-02-07 22:52:42 +0100

    Build system: generate metadata.json
    Instead of hardcoding metadata.json, generate it from .in files,
    adding installation prefixes, current target version and configured
    uuid.

--- extension.mk
+++ extension.mk
@@ -1,10 +1,19 @@
 # Change these to modify how installation is performed
-# If you modify extensionbase, you also need to modify
-# metadata.json of each extension
 topextensiondir = $(datadir)/gnome-shell/extensions
 extensionbase = @gnome-shell-extensions.gnome.org
 
-extensiondir = $(topextensiondir)/$(EXTENSION_ID)$(extensionbase)
+uuid = $(EXTENSION_ID)$(extensionbase)
 
-dist_extension_DATA = metadata.json extension.js stylesheet.css
-nodist_extension_DATA = $(EXTRA_EXTENSION)
+extensiondir = $(topextensiondir)/$(uuid)
+
+dist_extension_DATA = extension.js stylesheet.css
+nodist_extension_DATA = metadata.json $(EXTRA_EXTENSION)
+
+EXTRA_DIST = metadata.json.in
+
+metadata.json: metadata.json.in
+	sed -e "s|[@]LOCALEDIR@|$(datadir)/locale|" \
+	    -e "s|[@]uuid@|$(uuid)|" \
+            -e "s|[@]shell_current@|$(PACKAGE_VERSION)|" $< > $@
+
+CLEANFILES = metadata.json
