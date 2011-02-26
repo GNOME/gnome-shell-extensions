fbbe5ba7640d70c04f5261a4c65e6ea3e243b4e0
Giovanni Campagna <gcampagna@src.gnome.org>
2011-02-26 16:41:52 +0100

    Add url property in the metadata
    GNOME Shell emits a warning when "url" is absent in metadata.json,
    adding it costs nothing. Links to the GIT repository because I
    found no better page to reference.

--- extension.mk
+++ extension.mk
@@ -1,3 +1,5 @@
+extensionurl = http://git.gnome.org/gnome-shell-extensions
+
 # Change these to modify how installation is performed
 topextensiondir = $(datadir)/gnome-shell/extensions
 extensionbase = @gnome-shell-extensions.gnome.org
@@ -14,6 +16,7 @@ EXTRA_DIST = metadata.json.in
 metadata.json: metadata.json.in $(top_builddir)/config.status
 	$(AM_V_GEN) sed -e "s|[@]LOCALEDIR@|$(datadir)/locale|" \
 	    -e "s|[@]uuid@|$(uuid)|" \
-            -e "s|[@]shell_current@|$(PACKAGE_VERSION)|" $< > $@
+	    -e "s|[@]shell_current@|$(PACKAGE_VERSION)|" \
+	    -e "s|[@]url@|$(extensionurl)|" $< > $@
 
 CLEANFILES = metadata.json
