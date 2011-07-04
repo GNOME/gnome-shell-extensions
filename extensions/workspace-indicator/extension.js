20094e43c514739871c3315f449794eee2b05356
Erick Pérez Castellanos <erick.red@gmail.com>
2011-07-04 08:39:19 -0400

    New extension: workspace-indicator
    This extension shows a menu in the status area that indicates
    current workspace and allows for quick switching, similar to the
    workspace-indicator applet in GNOME 2.

---
+++ extensions/workspace-indicator/extension.js
@@ -0,0 +1,98 @@
+const Meta = imports.gi.Meta;
+const Clutter = imports.gi.Clutter;
+const St = imports.gi.St;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Panel = imports.ui.panel;
+
+const Main = imports.ui.main;
+
+const Gettext = imports.gettext.domain('gnome-shell-extensions');
+const _ = Gettext.gettext;
+
+function WorkspaceIndicator() {
+	this._init.apply(this, arguments);
+}
+
+WorkspaceIndicator.prototype = {
+	__proto__: PanelMenu.SystemStatusButton.prototype,
+
+	_init: function(){
+		PanelMenu.SystemStatusButton.prototype._init.call(this, 'folder');
+
+		this.statusLabel = new St.Label({ text: this._labelText() });
+		this.actor.set_child(this.statusLabel);
+
+		this.workspacesItems = [];
+		this._workspaceSection = new PopupMenu.PopupMenuSection();
+		this.menu.addMenuItem(this._workspaceSection);
+		global.screen.connect_after('workspace-added', Lang.bind(this,this._createWorkspacesSection));
+		global.screen.connect_after('workspace-removed', Lang.bind(this,this._createWorkspacesSection));
+		global.screen.connect_after('workspace-switched', Lang.bind(this,this._updateIndicator));
+		this.actor.connect('scroll-event', Lang.bind(this, this._onScrollEvent));
+		this._createWorkspacesSection();
+
+		//styling
+		this.menu.actor.add_style_class_name('workspace-indicator-shorter');
+	},
+
+	_updateIndicator: function() {
+		this.statusLabel.set_text(this._labelText());
+	},
+
+	_labelText : function(workspaceIndex) {
+		if(workspaceIndex == undefined) {
+			workspaceIndex = global.screen.get_active_workspace().index();
+		}
+		return Meta.prefs_get_workspace_name(workspaceIndex);
+	},
+
+	_createWorkspacesSection : function() {
+		this._workspaceSection.removeAll();
+		this.workspacesItems = [];
+
+		let i = 0;
+		for(; i < global.screen.n_workspaces; i++) {
+			this.workspacesItems[i] = new PopupMenu.PopupMenuItem(this._labelText(i));
+			this._workspaceSection.addMenuItem(this.workspacesItems[i]);
+			this.workspacesItems[i].workspaceId = i;
+			this.workspacesItems[i].label_actor = this.statusLabel;
+			let self = this;
+			this.workspacesItems[i].connect('activate', Lang.bind(this, function(actor, event) {
+				this._activate(actor.workspaceId);
+			}));
+		}
+		if(i == 1) {
+			this._updateIndicator();
+		}
+	},
+
+	_activate : function (index) {
+		if(index >= 0 && index <  global.screen.n_workspaces) {
+			let metaWorkspace = global.screen.get_workspace_by_index(index);
+			metaWorkspace.activate(true);
+		}
+	},
+
+	_onScrollEvent : function(actor, event) {
+		let direction = event.get_scroll_direction();
+		let diff = 0;
+		if (direction == Clutter.ScrollDirection.DOWN) {
+			diff = 1;
+		} else if (direction == Clutter.ScrollDirection.UP) {
+			diff = -1;
+		} else {
+			return;
+		}
+
+		let newIndex = global.screen.get_active_workspace().index() + diff;
+		this._activate(newIndex);
+	},
+}
+
+function main() {
+	Panel.STANDARD_TRAY_ICON_ORDER.unshift('workspace-indicator');
+	Panel.STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION['workspace-indicator'] = WorkspaceIndicator;
+}
