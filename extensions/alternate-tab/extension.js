GNOME/gnome-shell-extensions

commit a78a16b4a7a255c7e4304c97b08160d451831e29

alternate-tab: respect aspect ratio for window thumbnails
It is weird if 16:9 thumbnails get turned into squares. Instead,
center them.

https://bugzilla.gnome.org/show_bug.cgi?id=656217

extensions/alternate-tab/extension.js
@@ -341,6 +341,8 @@ const WindowIcon = new Lang.Class({
             this.clone = new Clutter.Clone({ source: windowTexture,
 					     width: width * scale,
 					     height: height * scale,
+					     x_align: Clutter.ActorAlign.CENTER,
+					     y_align: Clutter.ActorAlign.CENTER,
 					     // usual hack for the usual bug in ClutterBinLayout...
 				             x_expand: true,
 					     y_expand: true });
@@ -353,6 +355,8 @@ const WindowIcon = new Lang.Class({
             this.clone = new Clutter.Clone({ source: windowTexture,
 					     width: width * scale,
 					     height: height * scale,
+					     x_align: Clutter.ActorAlign.CENTER,
+					     y_align: Clutter.ActorAlign.CENTER,
 					     // usual hack for the usual bug in ClutterBinLayout...
 				             x_expand: true,
 					     y_expand: true });
@@ -381,7 +385,7 @@ const WindowList = new Lang.Class({
     Extends: AltTab.SwitcherList,
 
     _init : function(windows, settings) {
-        this.parent(true);
+        this.parent(false);
 
         this.windows = windows;
         this.icons = [];

commit c5181cda4635af8f3e7187518966795245823171

alternate-tab: make the highlight square again
Fixes a regression from a78a16b4a7a255c7e4304c97b08160d451831e29

extensions/alternate-tab/extension.js
@@ -395,7 +395,7 @@ const WindowList = new Lang.Class({
     Extends: AltTab.SwitcherList,
 
     _init : function(windows, settings) {
-        this.parent(false);
+        this.parent(true);
 
         this.windows = windows;
         this.icons = [];