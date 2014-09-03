2667b9f3e58323606c4215f369896023c351d9db
Florian Müllner <fmuellner@gnome.org>
2014-09-03 15:11:19 +0200

    launch-new-instance: Update for gnome-shell changes

--- extensions/launch-new-instance/extension.js
+++ extensions/launch-new-instance/extension.js
@@ -4,28 +4,20 @@ const AppDisplay = imports.ui.appDisplay;
 var _onActivateOriginal = null;
 var _activateResultOriginal = null;
 
-function _onActivate(event) {
+function _activate(button) {
+  this.animateLaunch();
   this.app.open_new_window(-1);
   Main.overview.hide();
-}
-
-function _activateResult(result) {
-  let app = this._appSys.lookup_app(result);
-  app.open_new_window(-1);
 }
 
 function init() {
 }
 
 function enable() {
-  _onActivateOriginal = AppDisplay.AppIcon.prototype._onActivate;
-  AppDisplay.AppIcon.prototype._onActivate = _onActivate;
-
-  _activateResultOriginal = AppDisplay.AppSearchProvider.prototype.activateResult;
-  AppDisplay.AppSearchProvider.prototype.activateResult = _activateResult;
+  _activateOriginal = AppDisplay.AppIcon.prototype.activate;
+  AppDisplay.AppIcon.prototype.activate = _activate;
 }
 
 function disable() {
-  AppDisplay.AppIcon.prototype._onActivate = _onActivateOriginal;
-  AppDisplay.AppSearchProvider.prototype.activateResult = _activateResultOriginal;
+  AppDisplay.AppIcon.prototype.activate = _activateOriginal;
 }
