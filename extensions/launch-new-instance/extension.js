54e39c9779308643d77a14c16613d3ce478bad06
Florian Müllner <fmuellner@gnome.org>
2014-09-27 13:28:00 +0200

    launch-new-instance: Re-use original activate function
    Rather than re-implementing the function (and risk missing improvements
    like the launch animation), call the original one as if the user had
    middle-clicked the launcher.

--- extensions/launch-new-instance/extension.js
+++ extensions/launch-new-instance/extension.js
@@ -2,18 +2,14 @@ const AppDisplay = imports.ui.appDisplay;
 
 var _onActivateOriginal = null;
 
-function _activate(button) {
-  this.animateLaunch();
-  this.app.open_new_window(-1);
-  Main.overview.hide();
-}
-
 function init() {
 }
 
 function enable() {
   _activateOriginal = AppDisplay.AppIcon.prototype.activate;
-  AppDisplay.AppIcon.prototype.activate = _activate;
+  AppDisplay.AppIcon.prototype.activate = function() {
+      _activateOriginal.call(this, 2);
+  };
 }
 
 function disable() {
