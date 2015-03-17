0b18b15fee495df59fc561b59cb041a850e47ce5
Florian Müllner <fmuellner@gnome.org>
2015-03-17 19:45:27 +0100

    screenshot-window-size: Update for mutter API changes

--- extensions/screenshot-window-sizer/extension.js
+++ extensions/screenshot-window-sizer/extension.js
@@ -80,7 +80,7 @@ function cycleScreenshotSizes(display, screen, window, binding) {
         window.unmaximize(Meta.MaximizeFlags.HORIZONTAL | Meta.MaximizeFlags.VERTICAL);
 
     let workArea = window.get_work_area_current_monitor();
-    let outerRect = window.get_outer_rect();
+    let outerRect = window.get_frame_rect();
 
     // Find the nearest 16:9 size for the current window size
     let nearestIndex;
@@ -118,7 +118,7 @@ function cycleScreenshotSizes(display, screen, window, binding) {
 
     window.move_resize_frame(true, newX, newY, newWidth, newHeight);
 
-    let newOuterRect = window.get_outer_rect();
+    let newOuterRect = window.get_frame_rect();
     let message = newOuterRect.width + 'x' + newOuterRect.height;
 
     // The new size might have been constrained by geometry hints (e.g. for
