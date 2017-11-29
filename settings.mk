ba380190499711ff092c66d831d131af26a14368
Florian Müllner <fmuellner@gnome.org>
2017-11-29 21:49:09 +0100

    build: Remove autotools support
    Complete the switch to meson and drop autotools support ...
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-shell-extensions/issues/31

--- settings.mk
+++
@@ -1,9 +0,0 @@
-gsettings_SCHEMAS = $(gschemaname).gschema.xml
-
-%.desktop:%.desktop.in
-	$(AM_V_GEN) $(MSGFMT) --desktop --template $< -d $(top_srcdir)/po -o $@
-
-@GSETTINGS_RULES@
-
-CLEANFILES += $(gsettings_SCHEMAS:.xml=.valid)
-EXTRA_DIST += $(gsettings_SCHEMAS)
