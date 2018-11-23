371c278874c8ad28b608c9df78aa9759013701c1
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2018-11-23 14:01:13 +0300

    display application menu when no hotCorner
    application menu is unfunctional when hotCorner is undefined, for example after applying path https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/45

--- extensions/apps-menu/extension.js
+++ extensions/apps-menu/extension.js
@@ -240,17 +240,21 @@ class ApplicationsMenu extends PopupMenu.PopupMenu {
     }
 
     open(animate) {
-        this._button.hotCorner.setBarrierSize(0);
-        if (this._button.hotCorner.actor) // fallback corner
-            this._button.hotCorner.actor.hide();
+        if (this._button.hotCorner) {
+            this._button.hotCorner.setBarrierSize(0);
+            if (this._button.hotCorner.actor) // fallback corner
+                this._button.hotCorner.actor.hide();
+        }
         super.open(animate);
     }
 
     close(animate) {
         let size = Main.layoutManager.panelBox.height;
-        this._button.hotCorner.setBarrierSize(size);
-        if (this._button.hotCorner.actor) // fallback corner
-            this._button.hotCorner.actor.show();
+        if (this._button.hotCorner) {
+            this._button.hotCorner.setBarrierSize(size);
+            if (this._button.hotCorner.actor) // fallback corner
+                this._button.hotCorner.actor.show();
+        }
         super.close(animate);
     }
 
