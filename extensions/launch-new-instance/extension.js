3effc9cfc282f897d39b6b952e7d47fdfb0f8e17
Florian Müllner <fmuellner@gnome.org>
2019-01-26 22:05:50 +0100

    style: Fix indentation errors
    While we have some style inconsistencies - mostly regarding split lines,
    i.e. aligning to the first arguments vs. a four-space indent - there are
    a couple of places where the spacing is simply wrong. Fix those.
    
    Spotted by eslint.
    
    https://gitlab.gnome.org/GNOME/gnome-shell-extensions/merge_requests/49

--- extensions/launch-new-instance/extension.js
+++ extensions/launch-new-instance/extension.js
@@ -3,12 +3,12 @@ const AppDisplay = imports.ui.appDisplay;
 let _activateOriginal = null;
 
 function enable() {
-  _activateOriginal = AppDisplay.AppIcon.prototype.activate;
-  AppDisplay.AppIcon.prototype.activate = function() {
-      _activateOriginal.call(this, 2);
-  };
+    _activateOriginal = AppDisplay.AppIcon.prototype.activate;
+    AppDisplay.AppIcon.prototype.activate = function() {
+        _activateOriginal.call(this, 2);
+    };
 }
 
 function disable() {
-  AppDisplay.AppIcon.prototype.activate = _activateOriginal;
+    AppDisplay.AppIcon.prototype.activate = _activateOriginal;
 }
