60c75e5fcfda19a66d4dcf8c5135b4c56fb7beba
Florian Müllner <fmuellner@gnome.org>
2019-06-05 03:31:16 +0000

    classic: Add 'horizontal-workspaces' extension
    Vertical workspaces are another defining characteristics of GNOME 3,
    and thus rather un-classic. That switch was driven by the overall
    layout of the overview, and now that we disable the overview in
    GNOME Classic, we can just return to the traditional workspace
    layout as well.
    
    Add a small extension that does just that.
    
    https://gitlab.gnome.org/GNOME/gnome-shell-extensions/merge_requests/72

---
+++ extensions/horizontal-workspaces/extension.js
@@ -0,0 +1,38 @@
+/* exported init */
+const { Meta } = imports.gi;
+
+const { ThumbnailsBox } = imports.ui.workspaceThumbnail;
+
+class Extension {
+    constructor() {
+        this._origUpdateSwitcherVisibility =
+            ThumbnailsBox.prototype._updateSwitcherVisibility;
+    }
+
+    enable() {
+        global.workspace_manager.override_workspace_layout(
+            Meta.DisplayCorner.TOPLEFT,
+            false,
+            1,
+            -1);
+
+        ThumbnailsBox.prototype._updateSwitcherVisibility = function() {
+            this.hide();
+        };
+    }
+
+    disable() {
+        global.workspace_manager.override_workspace_layout(
+            Meta.DisplayCorner.TOPLEFT,
+            false,
+            -1,
+            1);
+
+        ThumbnailsBox.prototype._updateSwitcherVisibility =
+            this._origUpdateSwitcherVisibility;
+    }
+}
+
+function init() {
+    return new Extension();
+}
