eb517c851777067087c3bf067c2baf10dcaf4128
Neal Gompa <ngompa@fedoraproject.org>
2021-10-29 09:37:33 -0400

    classic: Install the session for Wayland and ship override sessions
    The regular GNOME session ships with three options:
    
    * GNOME
    * GNOME on Wayland (available when GDM starts in X11)
    * GNOME on Xorg (available when GDM starts in Wayland)
    
    The main GNOME session is set up so it works to match how GDM starts,
    so GNOME is on Wayland if GDM is (or GNOME is on X11 if GDM is).
    
    For GNOME Classic, we are missing this setup, so port this behavior
    over from the GNOME session setup.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/195>

---
+++ meson/session-post-install.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+if os.environ.get('DESTDIR'):
+  install_root = os.environ.get('DESTDIR') + os.path.abspath(sys.argv[1])
+else:
+  install_root = sys.argv[1]
+
+# FIXME: Meson is unable to copy a generated target file:
+#        https://groups.google.com/forum/#!topic/mesonbuild/3iIoYPrN4P0
+dst_dir = os.path.join(install_root, 'wayland-sessions')
+if not os.path.exists(dst_dir):
+  os.makedirs(dst_dir)
+
+src = os.path.join(install_root, 'xsessions', 'gnome-classic.desktop')
+dst = os.path.join(dst_dir, 'gnome-classic.desktop')
+shutil.copyfile(src, dst)
