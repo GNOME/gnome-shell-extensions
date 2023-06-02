b7895ad956248bbfb6622ed5838f8bc152401032
Florian Müllner <fmuellner@gnome.org>
2023-06-02 14:37:30 +0200

    extensions: Add new light-style extension
    Now that gnome-shell supports a light style, people may want to
    use it without forcing all apps to be light.
    
    Add a small extension that switches the default to light, so the
    shell follows the regular "dark style" preference.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/256>

---
+++ extensions/light-style/extension.js
@@ -0,0 +1,45 @@
+/* exported init */
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+const {St} = imports.gi;
+
+const Main = imports.ui.main;
+
+class Extension {
+    constructor() {
+        this._savedColorScheme = Main.sessionMode.colorScheme;
+    }
+
+    _updateColorScheme(scheme) {
+        Main.sessionMode.colorScheme = scheme;
+        St.Settings.get().notify('color-scheme');
+    }
+
+    enable() {
+        this._updateColorScheme('prefer-light');
+    }
+
+    disable() {
+        this._updateColorScheme(this._savedColorScheme);
+    }
+}
+
+/** */
+function init() {
+    return new Extension();
+}
