GNOME/gnome-shell-extensions

commit f576f1b1b67cf78be8d63353ee795f68cfa67446

window-list: Initialize preview visibility
Otherwise we can end up showing window previews in the workspace
thumbnail that shouldn't be visible, if none of the properties we
track for updates changes.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/280>

extensions/window-list/workspaceIndicator.js
@@ -43,6 +43,7 @@ class WindowPreview extends St.Button {
             },
             'notify::minimized', this._updateVisible.bind(this),
             this);
+        this._updateVisible();
 
         global.display.connectObject('notify::focus-window',
             this._onFocusChanged.bind(this), this);

commit 8b7cfff558befbacf6669364f669eba24e71eb98

window-list: Only initialize preview visibility on map
The check whether the window overlaps with the monitor the actor
is on requires the actor to be added to the stage first, so delay
the initial visibility update that was added in commit f576f1b1b6.

Fixes: f576f1b1 ("window-list: Initialize preview visibility")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/286>

extensions/window-list/workspaceIndicator.js
@@ -43,7 +43,6 @@ class WindowPreview extends St.Button {
             },
             'notify::minimized', this._updateVisible.bind(this),
             this);
-        this._updateVisible();
 
         global.display.connectObject('notify::focus-window',
             this._onFocusChanged.bind(this), this);
@@ -53,6 +52,12 @@ class WindowPreview extends St.Button {
     // needed for DND
     get metaWindow() {
         return this._window;
+    }
+
+    vfunc_map() {
+        super.vfunc_map();
+        // initialize visibility after being added to the stage
+        this._updateVisible();
     }
 
     _onFocusChanged() {