GNOME/gnome-shell-extensions

commit b9d2a445b2b9ff3dea771acd557b7f7c55eced7c

workspace-indicator: Initialize preview visibility
Otherwise we can end up showing windows that shouldn't be visible
if none of the properties we track for updates changes.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/280>

extensions/workspace-indicator/extension.js
@@ -49,6 +49,7 @@ class WindowPreview extends St.Button {
             },
             'notify::minimized', this._updateVisible.bind(this),
             this);
+        this._updateVisible();
 
         global.display.connectObject('notify::focus-window',
             this._onFocusChanged.bind(this), this);

commit 25a75e1b0e51d472230c914f3cdcda31a1ff8b8a

workspace-indicator: Only initialize preview visibility on map
The check whether the window overlaps with the monitor the actor
is on requires the actor to be added to the stage first, so delay
the initial visibility update that was added in commit b9d2a445b2.

Fixes: b9d2a445 ("workspace-indicator: Initialize preview visibility")
Closes:
https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/issues/469

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/286>

extensions/workspace-indicator/extension.js
@@ -49,7 +49,6 @@ class WindowPreview extends St.Button {
             },
             'notify::minimized', this._updateVisible.bind(this),
             this);
-        this._updateVisible();
 
         global.display.connectObject('notify::focus-window',
             this._onFocusChanged.bind(this), this);
@@ -59,6 +58,12 @@ class WindowPreview extends St.Button {
     // needed for DND
     get metaWindow() {
         return this._window;
+    }
+
+    vfunc_map() {
+        super.vfunc_map();
+        // initialize visibility after being added to the stage
+        setTimeout(() => this._updateVisible());
     }
 
     _onFocusChanged() {