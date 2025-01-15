GNOME/gnome-shell-extensions

commit 981e8e42e20338b5488fefddf291970caf525bfc

window-list: Untrack chrome while in overview
The window list is set up to track the monitor's fullscreen state.
Monitors are never considered in fullscreen while showing the overview,
so if shell's layout manager updates the fullscreen visibility after
we hid the bottom bar, it ends up being visible in the overview.

To avoid this, untrack the actor while the overview is visible.

Closes: https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/issues/509
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/364>

extensions/window-list/extension.js
@@ -903,10 +903,11 @@ class WindowList extends St.Widget {
             () => this._onWorkspaceMenuSet(), this);
         this._onWorkspaceMenuSet();
 
-        Main.layoutManager.addChrome(this, {
+        const chromeOptions = {
             affectsStruts: true,
             trackFullscreen: true,
-        });
+        };
+        Main.layoutManager.addChrome(this, chromeOptions);
         Main.uiGroup.set_child_above_sibling(this, Main.layoutManager.panelBox);
         Main.ctrlAltTabManager.addGroup(this, _('Window List'), 'start-here-symbolic');
 
@@ -943,10 +944,12 @@ class WindowList extends St.Widget {
 
         Main.overview.connectObject(
             'showing', () => {
+                Main.layoutManager.untrackChrome(this);
                 this.hide();
                 this._updateKeyboardAnchor();
             },
             'hidden', () => {
+                Main.layoutManager.trackChrome(this);
                 this.visible = !this._monitor.inFullscreen;
                 this._updateKeyboardAnchor();
             }, this);

commit 01f7df1e8a2de1340589dbcd47f98ff6a1744035

window-list: Use correct params when re-tracking chrome
Since commit 981e8e42, we temporarily untrack the window-list actor
while in the overview.

However as we don't pass pass the chrome parameters when re-tracking
chrome, the window-list no longer contributes to struts or tracks
fullscreen changes, whoops.

Make sure to pass the original parameters when re-tracking chrome
to restore the expected behavior.

Fixes: 981e8e42 ("window-list: Untrack chrome while in overview")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/issues/550
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/379>

extensions/window-list/extension.js
@@ -1029,7 +1029,7 @@ class WindowList extends St.Widget {
                 this._updateKeyboardAnchor();
             },
             'hidden', () => {
-                Main.layoutManager.trackChrome(this);
+                Main.layoutManager.trackChrome(this, chromeOptions);
                 this.visible = !this._monitor.inFullscreen;
                 this._updateKeyboardAnchor();
             }, this);