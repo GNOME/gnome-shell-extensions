29d11e5879410266ce166a5342eb2540478bbd4b
Florian Müllner <fmuellner@gnome.org>
2025-03-03 16:18:52 +0100

    screenshot-window-sizer: Adjust to mutter change
    Mutter no longer hides the `event` parameter in Meta.KeyHandlerFuncs,
    so update the function to account for the additional parameter.
    
    Closes https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/issues/559
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/388>

--- extensions/screenshot-window-sizer/extension.js
+++ extensions/screenshot-window-sizer/extension.js
@@ -63,9 +63,10 @@ export default class ScreenshotWindowSizerExtension extends Extension {
     /**
      * @param {Meta.Display} display - the display
      * @param {Meta.Window=} window - for per-window bindings, the window
+     * @param {Clutter.Event} event - the triggering event
      * @param {Meta.KeyBinding} binding - the key binding
      */
-    _cycleScreenshotSizes(display, window, binding) {
+    _cycleScreenshotSizes(display, window, event, binding) {
         const backwards = binding.is_reversed();
 
         // Unmaximize first
