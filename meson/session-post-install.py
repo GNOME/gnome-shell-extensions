GNOME/gnome-shell-extensions

commit b05eb4eb6d89a42c224d783183854c4d01e9d3ac

build: Allow disabling the X11 session
This is consistent with gnome-session and the rest of the GNOME
session stack.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/396>

data/meson.build
@@ -2,13 +2,18 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+have_x11 = get_option('x11')
+
 session_desktop_base = 'gnome-classic'
 
 session_desktops = [
   session_desktop_base,
-  session_desktop_base + '-xorg',
   session_desktop_base + '-wayland',
 ]
+
+if have_x11
+  session_desktops += [session_desktop_base + '-xorg']
+endif
 
 foreach name : session_desktops
   session_desktop = name + '.desktop'

meson.options
@@ -20,3 +20,9 @@ option('classic_mode',
   value: false,
   description: 'Enable installing data files for classic mode.'
 )
+
+option('x11',
+  type: 'boolean',
+  value: true,
+  description: 'Enable X11 session support.'
+)

commit 2552c92fe1200b792ce20b39299efff3eee9fe86

build: Do not install anything in xsessions when X11 is disabled
Fixes: b05eb4eb6d ("build: Allow disabling the X11 session")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/400>

data/meson.build
@@ -26,7 +26,7 @@ foreach name : session_desktops
     #        There is a workaround in meson/session-post-install.py until proper
     #        solution arises:
     #        https://github.com/mesonbuild/meson/issues/2416
-    session_instdir = xsessiondir
+    session_instdir = wlsessiondir
   #session_instdir = [ xesssiondir, wlsessiondir ]
   endif
   i18n.merge_file(

meson.build
@@ -31,6 +31,8 @@ shell_version = ver_arr[0]
 
 uuid_suffix = '@gnome-shell-extensions.gcampax.github.com'
 
+have_x11 = get_option('x11')
+
 classic_extensions = [
   'apps-menu',
   'places-menu',
@@ -86,10 +88,12 @@ endforeach
 
 if classic_mode_enabled
   subdir('data')
-  meson.add_install_script(
-    'meson/session-post-install.py',
-    join_paths(get_option('prefix'), datadir),
-  )
+  if have_x11
+    meson.add_install_script(
+      'meson/session-post-install.py',
+      join_paths(get_option('prefix'), datadir),
+    )
+  endif
 endif
 
 subdir('extensions')

meson/session-post-install.py
@@ -15,10 +15,10 @@ else:
 
 # FIXME: Meson is unable to copy a generated target file:
 #        https://groups.google.com/forum/#!topic/mesonbuild/3iIoYPrN4P0
-dst_dir = os.path.join(install_root, 'wayland-sessions')
+dst_dir = os.path.join(install_root, 'xsessions')
 if not os.path.exists(dst_dir):
   os.makedirs(dst_dir)
 
-src = os.path.join(install_root, 'xsessions', 'gnome-classic.desktop')
+src = os.path.join(install_root, 'wayland-sessions', 'gnome-classic.desktop')
 dst = os.path.join(dst_dir, 'gnome-classic.desktop')
 shutil.copyfile(src, dst)