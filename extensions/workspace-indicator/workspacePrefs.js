b5ac041c1a6f250a92ba970cf14c09ad19491673
Florian Müllner <fmuellner@gnome.org>
2025-05-22 16:20:05 +0200

    workspace-indicator: Include workspace settings
    While the "Multitasking" panel in Settings already exposes workspace
    settings, it makes sense to expose them in our prefs dialog as well
    where they are more in context.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/merge_requests/405>

--- extensions/workspace-indicator/workspacePrefs.js
+++ extensions/workspace-indicator/workspacePrefs.js
@@ -55,6 +55,75 @@ class GeneralGroup extends Adw.PreferencesGroup {
     }
 }
 
+class BehaviorGroup extends Adw.PreferencesGroup {
+    static {
+        GObject.registerClass(this);
+    }
+
+    constructor() {
+        super({
+            title: _('Behavior'),
+        });
+
+        const dynamicCheck = new Gtk.CheckButton();
+        const dynamicRow = new Adw.ActionRow({
+            title: _('Dynamic'),
+            subtitle: _('Automatically removes empty workspaces.'),
+            activatable_widget: dynamicCheck,
+        });
+        dynamicRow.add_prefix(dynamicCheck);
+        this.add(dynamicRow);
+
+        const fixedCheck = new Gtk.CheckButton({
+            group: dynamicCheck,
+        });
+        const fixedRow = new Adw.ActionRow({
+            title: _('Fixed Number'),
+            subtitle: _('Specify a number of permanent workspaces.'),
+            activatable_widget: fixedCheck,
+        });
+        fixedRow.add_prefix(fixedCheck);
+        this.add(fixedRow);
+
+        const adjustment = new Gtk.Adjustment({
+            lower: 1,
+            step_increment: 1,
+            value: 4,
+            upper: 36, // hard limit in mutter
+        });
+        const numRow = new Adw.SpinRow({
+            title: _('Number of Workspaces'),
+            adjustment,
+        });
+        this.add(numRow);
+
+        const mutterSettings = new Gio.Settings({
+            schema_id: 'org.gnome.mutter',
+        });
+
+        if (mutterSettings.get_boolean('dynamic-workspaces'))
+            dynamicCheck.active = true;
+        else
+            fixedCheck.active = true;
+
+        mutterSettings.bind('dynamic-workspaces',
+            dynamicCheck, 'active',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        const desktopSettings = new Gio.Settings({
+            schema_id: 'org.gnome.desktop.wm.preferences',
+        });
+
+        desktopSettings.bind('num-workspaces',
+            numRow, 'value',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        fixedCheck.bind_property('active',
+            numRow, 'sensitive',
+            GObject.BindingFlags.SYNC_CREATE);
+    }
+}
+
 class NewItem extends GObject.Object {}
 GObject.registerClass(NewItem);
 
@@ -247,6 +316,7 @@ export class WorkspacesPage extends Adw.PreferencesPage {
         });
 
         this.add(new GeneralGroup(settings));
+        this.add(new BehaviorGroup());
         this.add(new WorkspacesGroup());
     }
 }
